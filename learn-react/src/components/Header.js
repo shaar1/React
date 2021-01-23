9950c5f1bb24dd3d07caffd8c77756a2160a3c7b
Abdallah <abdallahchaar90@gmail.comh>
2021-01-23 13:53:58 +0100

    m

--- learn-react/src/components/Header.js
+++ learn-react/src/components/Header.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 function Header(){
+let title="";
+const client = "Designer";
+
+if(client === "Designer"){
+    title = "Design is my life.";
+}else{
+    title = "Progamming is my life.";
+}
+
     return(
         <div className="navbar bg-dark rounded">
-        <h2 className="text-white"> Design is my life. </h2>  
-
+        <h2 className="text-white">{title}</h2>  
         </div>
     );
 }
